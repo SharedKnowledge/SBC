03beb1a05547d6f5b192699240efbb722c579df5
Michael Schwarz <schwarzm90@gmail.com>
2016-06-07 11:03:42 +0200

    Add list of broadcasts to fragment.

---
+++ app/src/main/java/net/sharksystem/sbc/adapters/WifiDirectBroadcastAdapter.java
@@ -0,0 +1,72 @@
+package net.sharksystem.sbc.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import net.sharkfw.asip.ASIPInterest;
+import net.sharkfw.knowledgeBase.SemanticTag;
+import net.sharkfw.knowledgeBase.SharkKBException;
+import net.sharksystem.android.protocols.wifidirect.WifiDirectPeer;
+import net.sharksystem.sbc.R;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by micha on 17.12.15.
+ */
+public class WifiDirectBroadcastAdapter extends BaseAdapter {
+
+    private List<String> _messages;
+    private Context mContext;
+
+    public WifiDirectBroadcastAdapter(Context context){
+        mContext = context;
+        _messages = new ArrayList<>();
+    }
+
+    public void setContext(Context context){
+        mContext = context;
+    }
+
+    @Override
+    public int getCount() {
+        return _messages.size();
+    }
+
+    @Override
+    public String getItem(int position) {
+        return _messages.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    public void add(String message){
+        _messages.add(message);
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        String message = getItem(position);
+
+        // Check if an existing view is being reused, otherwise inflate the view
+        if (convertView == null) {
+            convertView = LayoutInflater.from(mContext)
+                    .inflate(R.layout.list_item_broadcasts, parent, false);
+        }
+
+        TextView messageView = (TextView) convertView.findViewById(R.id.textViewBroadcastMessage);
+        messageView.setText(message);
+        return convertView;
+    }
+}
--- app/src/main/java/net/sharksystem/sbc/fragments/BroadcastsFragment.java
+++ app/src/main/java/net/sharksystem/sbc/fragments/BroadcastsFragment.java
@@ -1,24 +1,77 @@
 package net.sharksystem.sbc.fragments;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.LocalBroadcastManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 
 import net.sharksystem.android.peer.SharkServiceController;
+import net.sharksystem.android.protocols.wifidirect.WifiDirectKPNotifier;
+import net.sharksystem.sbc.Application;
 import net.sharksystem.sbc.R;
+import net.sharksystem.sbc.adapters.WifiDirectBroadcastAdapter;
+
+import java.util.ArrayList;
 
 public class BroadcastsFragment extends Fragment implements View.OnClickListener {
 
-//    private final SharkServiceController _serviceController;
+    private final SharkServiceController _serviceController;
+    private final WifiDirectBroadcastAdapter _broadCastAdapter;
+    private final BroadcastReceiver _broadcastReceiver;
     private EditText _editText;
     private Button _button;
+    private ArrayList<String> _broadCasts;
+    private ListView _listView;
+    private final Context _context;
 
     public BroadcastsFragment() {
-//        _serviceController = SharkServiceController.getInstance(getContext());
+        _context = Application.getAppContext();
+
+        _serviceController = SharkServiceController.getInstance(getContext());
+        _broadCasts = new ArrayList<>();
+        _broadCastAdapter = new WifiDirectBroadcastAdapter(_context);
+
+        _broadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String action = intent.getAction();
+
+                if(WifiDirectKPNotifier.NEW_BROADCAST_ACTION.equals(action)){
+//                    _broadCasts.add(intent.getStringExtra("broadcast_message"));
+                    _broadCastAdapter.add(intent.getStringExtra("broadcast_message"));
+                }
+            }
+        };
+
+
+//        IntentFilter intentFilter = new IntentFilter();
+//        intentFilter.addAction(WifiDirectKPNotifier.NEW_BROADCAST_ACTION);
+//        LocalBroadcastManager.getInstance(_context).registerReceiver(
+//                _broadcastReceiver, intentFilter);
+    }
+
+    @Override
+    public void onResume() {
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(WifiDirectKPNotifier.NEW_BROADCAST_ACTION);
+        LocalBroadcastManager.getInstance(_context).registerReceiver(
+                _broadcastReceiver, intentFilter);
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        LocalBroadcastManager.getInstance(_context).unregisterReceiver(_broadcastReceiver);
+        super.onPause();
     }
 
     @Override
@@ -29,15 +82,20 @@ public class BroadcastsFragment extends Fragment implements View.OnClickListener
         _editText = (EditText) view.findViewById(R.id.editTextBroadcast);
         _button = (Button) view.findViewById(R.id.buttonSendBroadcast);
         _button.setOnClickListener(this);
+
+        _listView = (ListView) view.findViewById(R.id.listViewBroadcasts);
+        _listView.setAdapter(_broadCastAdapter);
         return view;
     }
 
     @Override
     public void onClick(View v) {
-//        String broadcast = _editText.getText().toString();
+        String broadcast = _editText.getText().toString();
 //
-//        if(!broadcast.isEmpty()){
-//            _serviceController.sendBroadcast(broadcast);
-//        }
+        if(!broadcast.isEmpty()){
+            _serviceController.sendBroadcast(broadcast);
+        }
     }
+
+
 }
